a461f2a87e368a262db06b29083cfb9d73ac229e
Vermont <vermont.ironhack@gmail.com>
2018-12-07 11:51:08 -0600

    Testing automatic deploy

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Express - Automatic Deploy Test';
 
 
 
