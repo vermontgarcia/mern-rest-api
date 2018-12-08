ec19a3caac4b304fa132000dc6c7e24d6a657dfd
Vermont <vermont.ironhack@gmail.com>
2018-12-08 14:29:18 -0600

    Router Auth modified, change Username with Email

--- routes/auth.js
+++ routes/auth.js
@@ -36,12 +36,17 @@ authRouter.post('/signup', (req, res) => {
 
 
 authRouter.post('/login', async (req, res) => {
-  const user = await User.findOne({username: req.body.username});
+  const user = await User.findOne({email: req.body.email});
+
   if(!user) return res.status(404).json({msg: 'User is not registered'});
+  
   let validPassword = bcrypt.compareSync(req.body.password, user.password);
+  
   if(!validPassword) return res.status(500).json({msg: 'Wrong password'});
+  
   const token = jwt.sign({id: user._id}, process.env.SECRET);
   delete user._doc.password;
+  
   res.status(200).json({user, token, msg: 'Logged succesfully'});
 });
 
