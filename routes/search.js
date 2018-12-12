4f07579f457de702092d7573f23cdb37b89a4272
Vermont <vermont.ironhack@gmail.com>
2018-12-12 17:44:38 -0600

    Puppeteer first approuch

--- routes/search.js
+++ routes/search.js
@@ -13,7 +13,50 @@ searchRouter.get('/:product', (req, res) => {
 
   //console.log('Back',req.params.product)
 
-  res.status(200).json({msg: req.params.product})
+  ///// Search Product Script
+
+  (async()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.goto('https://www.superama.com.mx');
+    
+    // Type into search box.
+    //await page.type('#searchbox input', 'tortillinas');
+    await page.type('#searchBox input', 'tortillinas');
+    
+    // Wait for suggest overlay to appear and click "show all results".
+    const searchButton = '.barraDeBusquedaButton';
+    await page.waitForSelector(searchButton);
+    await page.click(searchButton);
+    
+    // Wait for the results page to load and display the results.
+    const resultsSelector = '.isotope-item';
+    
+    await page.waitForSelector(resultsSelector);
+    
+    // Extract the results from the page.
+    
+    const items = await page.evaluate(() =>
+      Array.from(document.querySelectorAll( '.isotope-item'))
+        .map(item => ({
+          image: item.querySelector('.upcImage img').src ,
+          name: item.querySelector('.upcName a').innerText.trim(),
+          price: item.querySelector('.upcPrice').innerText.trim()
+        })
+      )
+    )
+    
+    console.log(items);
+    
+    await browser.close();
+    
+    res.status(200).json({msg: req.params.product})
+  })();
+
+
+  ////////
+
   
 });
 
