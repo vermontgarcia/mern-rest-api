e1c2ec2f3f7ecd99c254b188a1ce696f36a9ef50
Vermont <vermont.ironhack@gmail.com>
2018-12-12 20:16:54 -0600

    Updating REACT App into API directory

--- public/precache-manifest.23f1050428e5d549d7fac4f6ce009a29.js
+++ public/precache-manifest.23f1050428e5d549d7fac4f6ce009a29.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "d1715bfb59722a1de497",
-    "url": "/static/js/main.d1715bfb.chunk.js"
+    "revision": "890ff73c6d884e06baeb",
+    "url": "/static/js/main.890ff73c.chunk.js"
   },
   {
     "revision": "6f12e43387f97c547b53",
     "url": "/static/js/1.6f12e433.chunk.js"
   },
   {
-    "revision": "d1715bfb59722a1de497",
+    "revision": "890ff73c6d884e06baeb",
     "url": "/static/css/main.380d132f.chunk.css"
   },
   {
-    "revision": "68331ee31ff5d6cea3055a5ea100d980",
+    "revision": "5be1a811f426393a786728b5a2a05f07",
     "url": "/index.html"
   }
 ];
