55eb2f0f04545f2ca9b8e0b113e24dd2709c8030
vermontgarcia <vermont.ironhack@gmail.com>
2018-12-13 20:09:59 -0600

    Superama refined search

--- routes/search.js
+++ routes/search.js
@@ -8,60 +8,35 @@ const searchRouter  = express.Router();
 
 searchRouter.get('/:product', (req, res) => {
   //console.log('Probando')
-
   if(!req.params.product) return res.status(500).json({msg: 'No product defined'});
-
   //console.log('Back',req.params.product)
-
-  ///// Search Product Script
-
+  //Search Product Script
   (async()=>{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
-    await page.goto(`https://www.superama.com.mx/buscar/${req.params.product}`);
-
-    /*
-    // Type into search box.
-    //await page.type('#searchbox input', 'tortillinas');
-    await page.type('#searchBox input', 'tortillinas');
-    
-    // Wait for suggest overlay to appear and click "show all results".
-    const searchButton = '.barraDeBusquedaButton';
-    await page.waitForSelector(searchButton);
-    await page.click(searchButton);
-    
-    */
-
-    // Wait for the results page to load and display the results.
-    const resultsSelector = '.isotope-item';
-    
-    await page.waitForSelector(resultsSelector);
-    
-    // Extract the results from the page.
-    
-    const items = await page.evaluate(() =>
+    try{
+      await page.goto(`https://www.superama.com.mx/buscar/${req.params.product}`);
+      // Wait for the results page to load and display the results.
+      const resultsSelector = '.isotope-item';
+      await page.waitForSelector(resultsSelector);
+      // Extract the results from the page.
+      const items = await page.evaluate(() =>
       Array.from(document.querySelectorAll( '.isotope-item'))
         .map(item => ({
           image: item.querySelector('.upcImage img').src ,
           name: item.querySelector('.upcName a').innerText.trim(),
           price: item.querySelector('.upcPrice').innerText.trim()
-        })
+          })
+        )
       )
-    )
-    
-    console.log(items);
-    
-    await browser.close();
-    
-    res.status(200).json({items, msg: 'Search executed succesfully'})
+      console.log(items);
+      await browser.close();
+      res.status(200).json({items, msg: 'Search executed succesfully'})
+    } catch (err) {
+      console.log('Error on Search', err)
+      res.status(500).json({msg:'Something whent wrong! Try again', err})
+    }
   })();
-
-
-  ////////
-
-  
 });
-
 
 module.exports = searchRouter;
