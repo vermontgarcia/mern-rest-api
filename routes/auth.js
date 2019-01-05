cd90504a08207c84ae2ae5698c775297ac1e79c4
vermontgarcia <vermont.ironhack@gmail.com>
2019-01-04 22:00:42 -0600

    Messages chenged to spanish

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const upload = require('../helpers/multer');
 /* GET & POST Auth routes */
 
 authRouter.post('/signup', (req, res) => {
-  if(req.body.password !== req.body.confirm) return res.status(500).json({msg: 'Passwords missmatch'});
+  if(req.body.password !== req.body.confirm) return res.status(500).json({msg: 'Upss! Las contraseñas no coinciden'});
 
   const salt = bcrypt.genSaltSync(256);
   const hashedPassword = bcrypt.hashSync(req.body.password, salt);
@@ -26,11 +26,11 @@ authRouter.post('/signup', (req, res) => {
   .then(user => {
     const token = jwt.sign({id: user._id}, process.env.SECRET);
     delete user._doc.password;
-    res.status(200).json({msg: 'User created succesfully', user, token});
+    res.status(200).json({msg: 'Listo! Ya eres parte de Compare.it', user, token});
   })
   .catch(err => {
     console.log('User SingUp Error =====>', err);
-    res.status(500).json({err, msg: 'User cannot be created'});
+    res.status(500).json({err, msg: 'Ouch! Este usuario ya esta registrado'});
   });
 });
 
@@ -38,16 +38,16 @@ authRouter.post('/signup', (req, res) => {
 authRouter.post('/login', async (req, res) => {
   const user = await User.findOne({email: req.body.email});
 
-  if(!user) return res.status(404).json({msg: 'User is not registered'});
+  if(!user) return res.status(404).json({msg: 'Upss! Este usuario no esta registrado'});
   
   let validPassword = bcrypt.compareSync(req.body.password, user.password);
   
-  if(!validPassword) return res.status(500).json({msg: 'Wrong password'});
+  if(!validPassword) return res.status(500).json({msg: 'Upss! La contraseña es incorrecta'});
   
   const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: 86400});
   delete user._doc.password;
   
-  res.status(200).json({user, token, msg: 'Logged succesfully'});
+  res.status(200).json({user, token, msg: 'Cool! Es bueno verte de regreso'});
 });
 
 authRouter.patch('/edit', upload.single('profilePicture'), (req, res) => {
@@ -59,27 +59,27 @@ authRouter.patch('/edit', upload.single('profilePicture'), (req, res) => {
   user.profilePicture = req.file.url;
   User.findByIdAndUpdate(req.body._id, {$set: user}, {new: true})
     .then(user => {
-      if(!user) return res.status(500).json({msg: 'User update Error'});
+      if(!user) return res.status(500).json({msg: 'Ouch! No encontramos este usuario'});
       delete user._doc.password;
-      res.status(200).json({user, msg: 'User updated succesfully'});
+      res.status(200).json({user, msg: 'Yes!!! Tus datos ya estan actalizados'});
     })
     .catch(err => {
       console.log('User update Error =====>', err);
-      res.status(500).json({err});
+      res.status(500).json({err, msg: 'Ouch! Algo salió mal y no pudimos actualizar tus datos'});
     });
 });
 
 authRouter.get('/loggedin', (req, res) => {
 
   const token = req.headers['x-access-token'];
-  if(!token) return res.status(403).json({msg:'No te pases, debes enviar un token'})
+  if(!token) return res.status(403).json({msg:'Token no recibido'})
 
   jwt.verify(token, process.env.SECRET, async (err, decoded)=>{
     //console.log('Decoded ====>', decoded.id)
-    if(err) return res.status(403).json({err, msg:'Sesion expirada'})
+    if(err) return res.status(403).json({err, msg:'Upss! esta sesion ya expiro, asi es mas seguro'})
     //req.user = await User.findById(decoded.id)
     //next();
-    res.status(200).json({msg: 'Valid token'})
+    res.status(200).json({msg: 'Perfecto! Este es un usuario valido'})
   })
 })
 
