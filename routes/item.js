a58e5cdcb9453d434d0a785b16e82789851d1e42
Vermont <vermont.ironhack@gmail.com>
2019-01-05 05:47:02 -0600

    searches log

--- routes/item.js
+++ routes/item.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Item = require('../models/Item')
+const Search = require('../models/Search')
 
 const itemRouter  = express.Router();
 
@@ -29,7 +30,33 @@ itemRouter.post('/', (req, res) => {
       console.log('Add Item Error =====>', err);
       res.status(500).json({err, msg: 'Ouch! Algo salió mal y no se agrego a tu lista'});
     });
-
 })
+
+itemRouter.get('/log/:id', (req, res) => {
+  console.log(req.params.id)
+
+  Search.find({ userId: req.params.id})
+    .then(searches => {
+      res.status(200).json({searches, msg: 'Searches retrieved succesfully'})
+    })
+    .catch(err => {
+      console.log('Get Searches Error =====>', err);
+      res.status(500).json({err, msg: 'Ouch! Algo salió mal y no se obtuvo tu historia de busquedas'});
+    })  
+});
+
+itemRouter.post('/log', (req, res) => {
+  //console.log('Item =====>',req.body)
+  Search.create(req.body)
+    .then(search => {
+      console.log('Search', search)
+      res.status(200).json({msg: 'Bien! Ya esta en tú historico'});
+    })
+    .catch(err => {
+      console.log('Add Search Error =====>', err);
+      res.status(500).json({err, msg: 'Ouch! Algo salió mal y no se agrego a tu historia de busquedas'});
+    });
+})
+
 
 module.exports = itemRouter;
